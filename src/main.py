909e075082cd33fb117c6f315c305dba93159ba8
kanggs1116 <kinggs2000@gmail.com>
2026-06-11 17:36:44 +0900

    chore: add production docker environment

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import time
+
+
+def main():
+    print("Coin auto trading app container started.", flush=True)
+
+    while True:
+        print("App is running...", flush=True)
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    main()
